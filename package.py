b0efc7325d2a971053d12a5097f8fa14c5f9eb88
JerryPearl <jerry.ma@pearlstudio.com>
2020-02-18 10:17:53 +0800

    init rez

---
+++ package.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+name = 'tk_framework_desktopserver'
+
+version = '1.3.9'
+
+description = 'tk-framework-desktopserver'
+
+authors = ['ShotgunSoftware']
+
+tools = []
+
+requires = []
+
+build_command = "python {root}/rezbuild.py {install}"
+
+
+def commands():
+    # Allows to override the bundled tk-framework-desktopserver and use any
+    # one you want. This disables updates.
+    env.SGTK_DESKTOP_SERVER_LOCATION = '{root}'
+
+
+format_version = 2
